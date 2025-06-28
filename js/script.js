2fba70434d8f0efe6034ca7a917ecaba3dda95e6
Prattycodes <prathamtetgure883@gmail.com>
2025-06-28 21:51:58 +0530

    updated code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,1 @@
 AOS.init();
-
-// Toggle Mobile Menu and hide/show upper navbar
-const toggleMenu = document.getElementById("menu-toggle");
-const mobileMenu = document.getElementById("mobile-menu");
-
-let menuOpen = false;
-
-toggleMenu.addEventListener("click", () => {
-  menuOpen = !menuOpen;
-  mobileMenu.classList.toggle("hidden", !menuOpen);
-});
